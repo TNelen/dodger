97674a3e6ae8b88c0e92775e724571924409561e
WouterJac <37212861+WouterJac@users.noreply.github.com>
2018-03-15 13:51:15 +0100

    BackButton
    Backbutton toegevoegd

--- OptionsMenu.java
+++ OptionsMenu.java
@@ -2,13 +2,29 @@ package SoftwareProject.main;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.MouseEvent;
 
 public class OptionsMenu extends Menu {
 
 	public OptionsMenu(Game game, Handler handler) {
 		super(game, handler);
+		buttons.add(new ClickButton(fnt2, "Back", Color.WHITE, Color.WHITE, Color.BLACK, 7, Game.HEIGHT/2+100));
 		
+	}
+	
+	public void mousePressed(MouseEvent e){
+		int mx = e.getX();
+		int my = e.getY();
 		
+		try {
+			if(game.getState()==Game.STATE.Help) {
+				if(mouseOver(mx,my,buttons.get(0).getRectVal()[0],buttons.get(0).getRectVal()[1],buttons.get(0).getRectVal()[2],buttons.get(0).getRectVal()[3])) {			//Op deze manier kan mouseOver toch gebruikt worden
+					game.setState(Game.STATE.Menu);
+				}
+			}
+		}catch(Exception p) {
+			System.out.println("Fuck NullPointerExceptions");
+		}
 	}
 	
 	public void render(Graphics g) {
